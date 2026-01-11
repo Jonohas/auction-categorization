19e40f07e05fa17c79b328e17b5242acb97b7c2e
Andreas Maerten <24669514+Yimura@users.noreply.github.com>
2026-01-11 22:49:39 +0100

    feat: add devcontainer files to workspace (#3)
    * feat: add devcontainer files to workspace
    
    * fix: package.json so Vite works in a DevContainer
    
    * fix: case sensitive import issue
    
    * chore: updated bun.lock
    
    * chore: remove unused files

--- src/entry-client.tsx
+++
@@ -1,6 +0,0 @@
-import { createStartClient } from "@tanstack/start/client";
-import { getRouter } from "./server/index";
-
-export const { router, hydration } = createStartClient({
-  router: getRouter(),
-});
